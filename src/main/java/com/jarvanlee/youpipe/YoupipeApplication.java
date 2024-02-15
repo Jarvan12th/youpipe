4e6881a1901b74a46d701f2c3a7496fa904141ce
Jarvan Lee <jarvan-jobs@outlook.com>
2024-02-15 13:07:30 +1100

    feat: initial commit

---
+++ src/main/java/com/jarvanlee/youpipe/YoupipeApplication.java
@@ -0,0 +1,13 @@
+package com.jarvanlee.youpipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoupipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoupipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jarvanlee/youpipe/YoupipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvanlee.youpipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoupipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
